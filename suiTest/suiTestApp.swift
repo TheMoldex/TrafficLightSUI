fb712b0d4f29b5a3ec43ffae745d586bb30a77bb
Denis Stashkov <themoldex@gmail.com>
2023-12-12 11:44:11 +0300

    Initial Commit

---
+++ suiTest/suiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  suiTestApp.swift
+//  suiTest
+//
+//  Created by Денис Сташков on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct suiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
